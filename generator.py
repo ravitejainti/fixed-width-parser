d408eb58219bbf502eaf90a0a9822e892be30e7f
Ravi Teja Inti <ravi@Mac.lan>
2025-02-02 21:01:53 -0500

    Final version: cleaned code, added new data examples

--- generator.py
+++ generator.py
@@ -13,9 +13,13 @@ encoding = spec.get('FixedWidthEncoding', 'utf-8')      # Encoding purpose as we
 
 # generating some sample data
 data = [
-    {"f1": "1", "f2": "Andrew", "f3": "A", "f4": "B", "f5": "andrew@demyst.com", "f6": "USA", "f7": "1234567890", "f8": "Newark", "f9": "Next to New York City", "f10": "Employee"},
-    {"f1": "2", "f2": "Tom", "f3": "T", "f4": "M", "f5": "tom@demyst.com", "f6": "MEX", "f7": "0987654321", "f8": "Boston", "f9": "a city in Massachusetts", "f10": "Vendor"}
+    {"f1": "1001", "f2": "Michael Jordan", "f3": "MJ", "f4": "J", "f5": "michael.jordan@nba.com", "f6": "USA", "f7": "3125557890", "f8": "Chicago", "f9": "Basketball Legend", "f10": "Retired"},
+    {"f1": "1002", "f2": "Emma Watson", "f3": "EW", "f4": "W", "f5": "emma.watson@hollywood.org", "f6": "UK", "f7": "2075554321", "f8": "London", "f9": "Actress & Activist", "f10": "Celebrity"},
+    {"f1": "1003", "f2": "Elon Musk", "f3": "EM", "f4": "M", "f5": "elon.musk@tesla.com", "f6": "USA", "f7": "6505551234", "f8": "Austin", "f9": "Tech Entrepreneur", "f10": "CEO"},
+    {"f1": "1004", "f2": "Olivia Brown", "f3": "OB", "f4": "B", "f5": "olivia.brown@edu.edu", "f6": "CAN", "f7": "4165556789", "f8": "Toronto", "f9": "", "f10": "Teacher"}
 ]
+
+
 
 # Let's define a function for making the match as per the required width, like truncating or padding
 def pad_or_truncate(text, width):
